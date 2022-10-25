44354b84c8c8d88fea29cf7e7128b15f4ebecaae
KheffyC <kheffy.cervantez@gmail.com>
2022-10-25 13:49:46 -0700

    add migrations for Training model

--- main_app/models.py
+++ main_app/models.py
@@ -2,6 +2,15 @@ from django.db import models
 from django.urls import reverse
 
 # Create your models here.
+
+BATTLES = (
+    ('W', 'vs Wild Pokemon'),
+    ('C', 'vs Civilian'),
+    ('G', 'vs Gym Leader'),
+    ('T', 'vs Trainer')
+)
+
+
 class Pokemon(models.Model):
     name = models.CharField(max_length=100)
     type = models.CharField(max_length=100)
@@ -13,7 +22,24 @@ class Pokemon(models.Model):
     
     def get_absolute_url(self):
         return reverse("pokemon_detail", kwargs={"pk": self.id})
+    
+class Training(models.Model):
+    date = models.DateTimeField('training time')
+    battle = models.CharField(
+        max_length=1,
+        choices=BATTLES,
+        default=BATTLES[0][0]
+    )
+    
+    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f'{self.get_battle_display()} on {self.date}'
+    
+    class Meta:
+        ordering = ['-date']
+        
+        
 
 class Item(models.Model):
     name = models.CharField(max_length=50)
